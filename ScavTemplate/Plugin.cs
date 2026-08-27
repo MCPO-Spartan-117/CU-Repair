a8b9d9a3340382b2a793879983293e016bfb9f05
Мастер Чиф <43174917+MCPO-Spartan-117@users.noreply.github.com>
2026-08-27 03:40:16 +0000

    Initial commit

---
+++ ScavTemplate/Plugin.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using MonoMod.RuntimeDetour;
+using UnityEngine;
+using CUCoreLib.ContentReload;
+using CUCoreLib.Data;
+using CUCoreLib.Helpers;
+using CUCoreLib.Registries;
+using CUCoreLib.Saving;
+using Newtonsoft.Json.Linq;
+
+namespace ModNamespace
+{
+    [BepInPlugin(ModGUID, ModName, ModVersion)]
+    [BepInDependency("net.cucorelib", BepInDependency.DependencyFlags.HardDependency)] 
+    public class Plugin : BaseUnityPlugin
+    {
+        public const string ModGUID = "com.yourName.modName";
+        public const string ModName = "My First Casualties: Unknown Mod"; // To change .dll name, change the name in vars.targets
+        public const string ModVersion = "1.0.0";
+
+        internal static new ManualLogSource Logger;
+        private readonly Harmony _harmony = new(ModGUID);
+        public static Plugin Instance { get; private set; } = null!;
+
+        public void Awake()
+        {
+            Logger = base.Logger;
+            Instance = this;
+
+            _harmony.PatchAll();
+            Logger.LogInfo($"Plugin {ModName} is loaded!");
+
+            DoStuff();
+            ContentReloadManager.EnableHotReload(ModGUID); // Required for hot reload, put any registry registrations in the RegisterReloadable method below
+            RegisterReloadable();
+        }
+
+        private void RegisterReloadable()
+        {
+    
+        }
+
+        private void DoStuff()
+        {
+            
+        }
+    }
+}
